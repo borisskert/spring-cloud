8b4cd90ea8fd146977236be1c1ecc791e7aa8478
Boris Skert <boris.skert@adorsys.de>
2017-08-31 13:53:16 +0200

    hello-world service: response text is configurable

--- helloworld/src/main/java/de/adorsys/springcloud/helloworld/HelloController.java
+++ helloworld/src/main/java/de/adorsys/springcloud/helloworld/HelloController.java
@@ -1,13 +1,23 @@
 package de.adorsys.springcloud.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${helloworld.text}")
+    private String text;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello cloudy world";
+        return "Hello cloudy world: " + text;
+    }
+
+
+    @RequestMapping(value = "/")
+    public String home() {
+        return "Hi!";
     }
 }
