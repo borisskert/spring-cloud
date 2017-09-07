1708e4dc0209e3965cb891596158f0f00e0d81d3
Boris Skert <boris.skert@adorsys.de>
2017-09-07 13:34:19 +0200

    reduce code in ribbon load balancer

--- ribbon/src/main/java/de/adorsys/springcloud/ribbon/HelloController.java
+++ ribbon/src/main/java/de/adorsys/springcloud/ribbon/HelloController.java
@@ -1,28 +1,13 @@
 package de.adorsys.springcloud.ribbon;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RibbonClient(name = "hello-world", configuration = RibbonConfiguration.class)
-public class HelloController {
+@FeignClient(value = "hello-world")
+public interface HelloController {
 
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
-
-    @Autowired
-    RestTemplate restTemplate;
-
-    @RequestMapping("/hello")
-    public String getHello() {
-        return this.restTemplate.getForObject("http://hello-world/hello", String.class);
-    }
+    @GetMapping("/hello")
+    String getHello();
 }
