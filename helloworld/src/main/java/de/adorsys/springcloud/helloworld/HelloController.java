6811c2b0458d3763a00fd21e7a0d6cd4151e10e2
Boris Skert <boris.skert@adorsys.de>
2017-09-08 16:33:32 +0200

    helloworld is getting config from config-server

--- helloworld/src/main/java/de/adorsys/springcloud/helloworld/HelloController.java
+++ helloworld/src/main/java/de/adorsys/springcloud/helloworld/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${helloworld.text}")
+    @Value("${helloworld.text:_abc_}")
     private String text;
 
     @RequestMapping("/hello")
