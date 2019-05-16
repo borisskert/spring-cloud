cabccc1d78eaa4abf3aa699dce29a40a54cc0c75
Boris Skert <boris.skert@adorsys.de>
2019-05-16 09:17:24 +0200

    Correct package names

--- ribbon/src/main/java/de/adorsys/springcloud/ribbon/HelloController.java
+++ ribbon/src/main/java/de/adorsys/springcloud/ribbon/HelloController.java
@@ -1,6 +1,6 @@
 package de.adorsys.springcloud.ribbon;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
